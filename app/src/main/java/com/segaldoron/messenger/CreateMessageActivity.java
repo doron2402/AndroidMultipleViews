d88b7a3117fb9ac47b5f71ab9bc33498e937c91a
Doron Segal <doron2402@gmail.com>
2015-11-29 22:11:20 -0800

    done

--- app/src/main/java/com/segaldoron/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/segaldoron/messenger/CreateMessageActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "message";
+    public static final String EXTRA_SUBJECT = "subject";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +18,18 @@ public class CreateMessageActivity extends AppCompatActivity {
     }
 
     public void onSendMessage(View view) {
+        //Message
         EditText messageView = (EditText) findViewById(R.id.message);
         String messageText = messageView.getText().toString();
+        //Subject
+        EditText subjectView = (EditText) findViewById(R.id.subject);
+        String subjectText = subjectView.getText().toString();
 
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        //Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
         intent.putExtra(EXTRA_MESSAGE, messageText);
+        intent.putExtra(EXTRA_SUBJECT, subjectText);
         startActivity(intent);
     }
 }
