3c0ccc189377b43857350ffe06ef7129a071e6a3
Doron Segal <doron2402@gmail.com>
2015-11-29 21:47:34 -0800

    starting

---
+++ app/src/main/java/com/segaldoron/messenger/CreateMessageActivity.java
@@ -0,0 +1,27 @@
+package com.segaldoron.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
